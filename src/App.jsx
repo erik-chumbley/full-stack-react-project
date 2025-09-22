46c5ae8ba8827b6e9056497c4a4d553142e3714f
erik-chumbley <132586820+erik-chumbley@users.noreply.github.com>
2025-09-22 13:10:30 -0400

    Revert "feat: created 'Post' component for frontend"
    This reverts commit 744e1921b4972c198115cea092383ee19e266a4c.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
-import { Post } from './components/Post.jsx'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-export function App() {
+function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <Post
-      title='Full-Stack React Projects'
-      contents="Let's become full-stack developers!"
-      author='Daniel Bugl'
-    />
+    <>
+      <div>
+        <a href='https://vitejs.dev' target='_blank' rel='noreferrer'>
+          <img src={viteLogo} className='logo' alt='Vite logo' />
+        </a>
+        <a href='https://react.dev' target='_blank' rel='noreferrer'>
+          <img src={reactLogo} className='logo react' alt='React logo' />
+        </a>
+      </div>
+      <h1>Vite + React + Node.js</h1>
+      <div className='card'>
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className='read-the-docs'>
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
   )
 }
+
+export default App
